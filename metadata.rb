c857e91af644cf5122abe00da1a4dc8294cca8f4
Tomoya Kabe <limit.usus@gmail.com>
2020-08-12 01:19:58 +0900

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,9 @@
 name             'pacman'
-maintainer       'Jesse R. Adams'
-maintainer_email 'jesse@techno-geeks.org'
+maintainer       'Tomoya Kabe'
+maintainer_email 'limit.usus@gmail.com'
 license          'Apache-2.0'
 description      'Updates package list for pacman and has LWRP for pacman groups'
 version          '1.1.1'
+
+issues_url 'https://github.com/limitusus/pacman/issues'
+source_url 'https://github.com/limitusus/pacman'
